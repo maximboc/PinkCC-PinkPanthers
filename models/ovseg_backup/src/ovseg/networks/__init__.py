682ec507d7e90b2c9d009a5d6ab4893355d1d766
Erwin <erwin.rodrigues@epita.fr>
2025-05-22 00:04:34 +0200

    feat: remove git submodule for our own copy

--- models/ovseg_backup/src/ovseg/networks/__init__.py
+++
@@ -1,4 +0,0 @@
-from __future__ import absolute_import
-from . import *
-from ovseg.networks.UNet import UNet
-from ovseg.networks.resUNet import UNetResEncoder, UNetResEncoderV2, UNetResDecoder, UResNet, UNetResStemEncoder, UNetResShuffleEncoder
